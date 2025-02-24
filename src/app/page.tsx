c2836657018d118ee40b9c7bde71eafef84a8b1a
pjohns80 <parkeriscool83@gmail.com>
2025-02-24 11:32:54 -0600

    Add source files

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <h1 className="text-5xl font-bold mb-4 text-center">
+        portfolio :3
+      </h1>
+      <p className="text-xl text-gray-600 text-center">
+      meow meow meow
+      </p>
+    </main>
+  );
+} 
